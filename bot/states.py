07d2ce24a490c27dbb6eaaa40c43e69dd09b3302
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-07-31 18:04:17 +0500

    Added new Auth  menu state

--- bot/states.py
+++ bot/states.py
@@ -3,6 +3,9 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class States(StatesGroup):
 
+    # Auth menu
+    auth_menu = State()
+
     # Main states
     new_task = State()
     my_tasks = State()
